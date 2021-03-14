c20b85e6c1be20b1a8b47e2335fc45c1263856bf
Slavomír Žiak <slavomir.ziak@nike.sk>
2021-03-14 17:55:47 +0100

    formatting, polishing

--- json-viewer/src/component/DataTable.js
+++ json-viewer/src/component/DataTable.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { DataGrid } from '@material-ui/data-grid';
 
 
 export default function DataTable({header, data}) {
 
-    const columns = header.map(h => { return { field: h, headerName: h, sortable: true, flex: 1};});
+    const columns = header.map(h => { 
+      return { 
+        field: h, 
+        headerName: h, 
+        sortable: true, 
+        flex: 1
+      };
+    });
     
-    columns.splice(0, 0, {field: 'index', headerName: '#'})
+    columns.splice(0, 0, {field: 'id', headerName: '#', sortable: false})
 
     const rows = data.map((item, index) => {
-      const row = header.reduce((acc, h) => { acc[h] = item[h]; return acc;}, {id: index});
-      row['index'] = index + 1;
-      return row;
+      return header.reduce((acc, h) => {
+            acc[h] = item[h]; 
+            return acc;
+          }, {
+          id: index + 1
+        });
     });
 
     return (
